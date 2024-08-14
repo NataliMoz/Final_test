93b7368228722bec162993b10ec3d628f6b134c8
Natalia Moz <moznatali117@gmail.com>
2024-08-14 16:03:23 +0300

    Исправила код на ввод массива

--- obj/Debug/net8.0/Finial_test.AssemblyInfo.cs
+++ obj/Debug/net8.0/Finial_test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Finial_test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+485591e56bdce5addf92c91c48dd8339e545811e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4112ef1660bed7caafd5c4d2a2960a1b1be7c6a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Finial_test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Finial_test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
